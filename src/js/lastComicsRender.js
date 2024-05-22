4235ff8fe6805d6f914a6ac6fe1898077bf9692f
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-05-22 18:14:28 +0300

    added footer, updated lastComics

--- src/js/lastComicsRender.js
+++ src/js/lastComicsRender.js
@@ -1,11 +1,34 @@
-import { getLastComics } from './lastComicsAPI';
-import { refs } from './refs';
-
-const lastComicsList = refs.lastComicsList;
+import { getLastComics } from './apiServices.js';
+import { refs } from './refs.js';
 
 export async function lastComicsRender() {
-  const { data } = await getLastComics();
-  console.log(data.results);
+  const {
+    data: { results },
+  } = await getLastComics();
+
+  const markup = results
+    .map(({ id, title, thumbnail, creators }) => {
+      const writers = creators.items
+        .filter(({ role }) => {
+          return role === 'writer';
+        })
+        .map(({ name }) => {
+          return name;
+        })
+        .join(', ');
+      return `<li class="last-comics-item" id=${id}>
+        <img
+          class="last-comics-item-photo"
+          src=${thumbnail.path}.${thumbnail.extension}
+          alt=${title}
+        />
+        <h3 class="last-comics-item-title">${title}</h3>
+        <p class="last-comics-item-author">${writers}</p>
+      </li>`;
+    })
+    .join('');
+
+  refs.lastComicsList.insertAdjacentHTML('beforeend', markup);
 }
 
 lastComicsRender();
