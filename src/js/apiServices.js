cf6e9e61a837b22d27a1ba3fd6899320d56b2ac2
Marharyta23 <144152456+Marharyta23@users.noreply.github.com>
2024-05-26 22:12:24 +0300

    fix the images path (hopefully)

--- src/js/apiServices.js
+++ src/js/apiServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import MD5 from 'crypto-js/md5';
 
-axios.defaults.baseURL = 'http://gateway.marvel.com';
+axios.defaults.baseURL = 'https://gateway.marvel.com';
 
 const PUBLIC_API_KEY = 'bb59b27f1efca5bf3b3c536649aa56a1';
 const PRIVATE_API_KEY = '6807fcecafaf9ad7d96911be2c5dcacb9d630a11';
